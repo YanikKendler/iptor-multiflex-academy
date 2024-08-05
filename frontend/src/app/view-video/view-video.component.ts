a3e5fdcec0c3e21b5d34324af64216d792a0452e
Yanik Kendler <70104756+elYanuki@users.noreply.github.com>
2024-08-05 15:45:14 +0200

    basic view video page

--- frontend/src/app/view-video/view-video.component.ts
+++ frontend/src/app/view-video/view-video.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {NavigationComponent} from "../navigation/navigation.component"
+import {StarIconComponent} from "../icons/star/star.icon.component"
+import {BookmarkIconComponent} from "../icons/bookmark/bookmark.icon.component"
 
 @Component({
   selector: 'app-view-video',
   standalone: true,
-  imports: [],
+  imports: [
+    NavigationComponent,
+    StarIconComponent,
+    BookmarkIconComponent
+  ],
   templateUrl: './view-video.component.html',
   styleUrl: './view-video.component.scss'
 })
 export class ViewVideoComponent {
-
+  title: string = 'Video Title';
+  description: string = 'Video Description';
 }
