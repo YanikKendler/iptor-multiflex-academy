309b9dd548141f67cdfaf716a3731b61e915ffd9
Michael Leisch <michael.leisch@gmx.at>
2024-08-06 09:36:21 +0200

    #9 implement settings page

--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -3,11 +3,15 @@ import {NavigationComponent} from "../navigation/navigation.component";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {faCirclePlay, faGear, faUsersGear} from '@fortawesome/free-solid-svg-icons';
 import {NgStyle} from "@angular/common";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [NavigationComponent, FontAwesomeModule, NgStyle],
+  imports: [NavigationComponent, FontAwesomeModule, NgStyle, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
