b2c19503cf1f02f9188961ecb6711c084b000032
Michael Leisch <michael.leisch@gmx.at>
2024-08-19 16:44:21 +0200

    bell and user icon

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,15 +1,42 @@
 import {Component, Input} from '@angular/core';
 import {RouterLink} from "@angular/router"
+import {
+  faBell,
+  faCirclePlay,
+  faCircleUser,
+  faEllipsis,
+  faGear,
+  faTrash,
+  faUser,
+  faUsersGear
+} from "@fortawesome/free-solid-svg-icons";
+import {CdkMenu, CdkMenuTrigger} from "@angular/cdk/menu";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {IconButtonComponent} from "../basic/icon-button/icon-button.component";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    CdkMenu,
+    FaIconComponent,
+    IconButtonComponent,
+    MatButton,
+    CdkMenuTrigger
   ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
   @Input() simple: boolean = false;
+    protected readonly faTrash = faTrash;
+    protected readonly faEllipsis = faEllipsis;
+  protected readonly faUser = faUser;
+  protected readonly faCircleUser = faCircleUser;
+  protected readonly faGear = faGear;
+  protected readonly faCirclePlay = faCirclePlay;
+  protected readonly faUsersGear = faUsersGear;
+  protected readonly faBell = faBell;
 }
