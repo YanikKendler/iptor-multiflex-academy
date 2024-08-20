3e0fab271b7f2171013c91de5158075a49b84a3a
Yanik Kendler <70104756+elYanuki@users.noreply.github.com>
2024-08-20 14:02:35 +0200

    hotfix: forgot import

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {Component, EventEmitter, inject, Input, Output, ViewChild} from '@angular/core';
 import {RouterLink} from "@angular/router"
 import {
   faBell,
